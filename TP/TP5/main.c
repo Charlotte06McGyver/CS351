44cd7a49a8e8b1bd9a6b95b95a08408979911f44
charlotte <charlotte.casarrubios@gmail.com>
2022-10-12 10:46:04 +0200

    polonaise inversee fonctionne

--- TP/TP5/main.c
+++ TP/TP5/main.c
@@ -11,8 +11,9 @@ int calcul_notation_polonaise (char *operation){
     int i = 0;
     int temp_a, temp_b, result;
     for (i=0; i<strlen(operation); i++){
-        if (operation[i]!='+' || operation[i]!='-' || operation[i]!='*' || operation[i]!='/'){
-            empiler(Pile_temp, operation[i]); //si on a un nombre, on l'empile dans notre pile temporaire
+        if (operation[i]!='+' && operation[i]!='-' && operation[i]!='*' && operation[i]!='/'){
+            empiler(Pile_temp, (operation[i]-48)); //si on a un nombre, on l'empile dans notre pile temporaire       
+        /*on fait -48 pour palier la valeur ascii*/
         }
         else { //si on a un operateur, on depile les elements qu'on execute entre eux et on stocke le resultat obtenu dans la pile
             if (operation[i]=='+'){ 
